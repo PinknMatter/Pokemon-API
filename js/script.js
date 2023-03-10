308cf01745948e52ddb5afe4ce2fe4e4c4ac40e2
Noah Kornberg <noahkornberg@gmail.com>
2023-03-09 22:47:47 -0500

    refigured grass
    make the grass a simplier

--- js/script.js
+++ js/script.js
@@ -31,11 +31,13 @@ let pokemons = ["bulbasaur", "charmander", "squirtle", "pikachu", "jigglypuff", 
    "slowking", "unown", "wobbuffet", "girafarig", "pineco", "forretress", "dunsparce", "gligar", "steelix", "snubbull", "granbull", "qwilfish", "scizor", "shuckle", "heracross",
    "sneasel", "teddiursa", "ursaring", "slugma", "magcargo", "swinub", "piloswine", "corsola", "remoraid", "octillery", "delibird", "mantine", "skarmory", "houndour", "houndoom",
    "phanpy", "donphan", "stantler", "smeargle", "tyrogue", "hitmontop", "smoochum", "elekid", "magby", "miltank", "slowking", "unown", "wobbuffet"];
+//Preload functions makes sure that all pokemon images are loaded before continuing 
 function preload() {
    for (let i = 0; i < pokemons.length; i++) {
       pokemon = loadJSON("https://pokeapi.co/api/v2/pokemon/" + pokemons[i], function (pokemontemp) {
+         let pokemonname = pokemons[i];
          images[i] = loadImage(pokemontemp.sprites.front_default);
-         particles.push(new pParticles(images[i], random(800), random(800)));
+         particles.push(new pParticles(images[i], random(800), random(800), pokemonname));
 
       })
    }
@@ -45,15 +47,13 @@ function preload() {
 
 
 function setup() {
+   //setsup the canvas creates grass particles
 
    createCanvas(800, 800);
 
-   print(images);
-   image(images[0], width / 2, height / 2, 100, 100);
-   for (let i = 0; i < height / 7; i++) {
-      for (let y = 0; y < width / 7; y++) {
-         grass.push(new Grass());
-      }
+   for (let i = 0; i < 6000; i++) {
+      grass.push(new Grass(i));
+
 
    }
 
@@ -62,101 +62,32 @@ function setup() {
 
 
 function draw() {
+   //refreshed the background draws the amount of particles for grass and images
    background(0, 150, 0);
    for (let i = 0; i < particles.length; i++) {
       particles[i].draw();
+
       // particles[i].update();
    }
    for (let i = 0; i < grass.length; i++) {
-      grass[i].move();
-      grass[i].display();
+      grass[i].update();
+      grass[i].place();
    }
 
 
 
 
 }
-class Grass {
-   constructor() {
-      this.start = createVector(random(width), random(height));
-      this.len = random(10, 30);
-      this.curvature = random(0.1, 0.5);
-      this.angle = random(-PI / 4, PI / 4);
-      this.hovered = false;
-      this.initialPos = this.start.copy();
-      this.hitboxRadius = this.len * 2;
-      this.color = random(100, 200);
-   }
 
-   move() {
-      if (this.hovered) {
-         let direction = p5.Vector.sub(this.start, createVector(mouseX, mouseY)).normalize();
-         let distance = p5.Vector.dist(this.start, createVector(mouseX, mouseY));
-         let force = map(distance, 0, 100, 0, 20);
-         let movement = direction.mult(force * 5);
-         this.start.add(movement);
-         this.angle += random(-0.01, 0.01);
-         if (this.start.x > width + this.len) {
-            this.start.x = -this.len;
-         }
-         if (this.start.x < -this.len) {
-            this.start.x = width + this.len;
-         }
-         if (this.start.y < -this.len) {
-            this.start.y = height + this.len;
-            this.angle = random(-PI / 4, PI / 4);
-         }
-      } else {
-         let returnSpeed = 0.1;
-         let dx = this.initialPos.x - this.start.x;
-         let dy = this.initialPos.y - this.start.y;
-         this.start.x += dx * returnSpeed;
-         this.start.y += dy * returnSpeed;
-      }
-   }
-
-   display() {
-      push();
-      translate(this.start.x, this.start.y);
-      rotate(this.angle + HALF_PI);
-      noStroke();
-
-      fill(0, this.color, 0);
-      beginShape();
-      vertex(0, 0);
-      quadraticVertex(this.len / 2, -this.len / 2, this.len, 0);
-      quadraticVertex(this.len / 2, this.len / 2, 0, 0);
-      endShape();
-      pop();
-   }
-
-   checkHovered() {
-      let mousePos = createVector(mouseX, mouseY);
-      let distToMouse = p5.Vector.dist(this.start, mousePos);
-      if (distToMouse < this.hitboxRadius) {
-         this.hovered = true;
-      } else {
-         this.hovered = false;
-      }
-   }
-}
-
-function mouseMoved() {
-   for (let i = 0; i < grass.length; i++) {
-      grass[i].checkHovered();
-   }
-}
-
-
-
-
-
-
+//constructor for pokemon images 
 class pParticles {
-   constructor(pokemonImage, x, y) {
+   constructor(pokemonImage, x, y, name) {
       this.x = x;
       this.y = y;
+      this.name = name;
+
       this.pokemonImage = pokemonImage;
+
 
       this.homeX = x;
       this.homeY = y;
@@ -166,29 +97,73 @@ class pParticles {
       image(this.pokemonImage, this.x, this.y);
    }
 
-   // update() {
+}
 
-   //    // calculates the distance between particles and the mouse
-   //    let mouseD = dist(this.x, this.y, mouseX, mouseY);
-   //    let mouseA = atan2(this.y - mouseY, this.x - mouseX);
+//constructor for grass 
+class Grass {
 
-   //    // calculates the distance between the particle and where its home value is
-   //    let homeD = dist(this.x, this.y, this.homeX, this.homeY);
-   //    let homeA = atan2(this.homeY - this.y, this.homeX - this.x);
+   constructor(x, y) {
+      this.x = random(height);
+      this.y = random(width);
+      this.color = random(100, 200);
 
-   //    //onstrains the amount the particles disperse as well as the velocity and force 
-   //    let mouseF = constrain(map(mouseD, 0, 70, 10, 0), 0, 20);
-   //    let homeF = map(homeD, 0, 100, 0, 4);
+      this.homeX = this.x;
+      this.homeY = this.y;
+      this.angle = random(-PI / 4, PI / 4);
+      this.len = random(10, 30);
 
-   //    //velocity of the particles
-   //    let vx = cos(mouseA) * mouseF;
-   //    vx += cos(homeA) * homeF;
+   }
 
-   //    let vy = sin(mouseA) * mouseF;
-   //    vy += sin(homeA) * homeF;
 
-   //    //add the particles position to the velocity
-   //    this.x += vx;
-   //    this.y += vy;
-   // }
+   update() {
+
+      // calculates the distance between particles and the mouse
+      let mouseD = dist(this.x, this.y, mouseX, mouseY);
+      let mouseA = atan2(this.y - mouseY, this.x - mouseX);
+
+      // calculates the distance between the particle and where its home value is
+      let homeD = dist(this.x, this.y, this.homeX, this.homeY);
+      let homeA = atan2(this.homeY - this.y, this.homeX - this.x);
+
+
+      //constrains the amount the particles disperse as well as the velocity and force 
+      let mouseF = constrain(map(mouseD, 10, 180, 10, 0), 0, 20);
+      let homeF = map(homeD, 0, 100, 0, 4);
+
+      //velocity of the particles
+      let vx = cos(mouseA) * mouseF;
+      vx += cos(homeA) * homeF;
+
+      let vy = sin(mouseA) * mouseF;
+      vy += sin(homeA) * homeF;
+
+      //add the particles position to the velocity
+      this.x += vx;
+      this.y += vy;
+
+
+   }
+
+   //draws grass shapehover() {
+
+   if(this.name = charzard) {
+      alert("You found him");
+   }
+}
+place() {
+   push();
+   translate(this.x, this.y);
+   rotate(this.angle + HALF_PI);
+   noStroke();
+   fill(50, this.color, 50);
+   beginShape();
+   vertex(0, 0);
+   quadraticVertex(this.len / 2, -this.len / 2, this.len, 0);
+   quadraticVertex(this.len / 2, this.len / 2, 0, 0);
+   endShape();
+   pop();
+
+}
+
+
 }
